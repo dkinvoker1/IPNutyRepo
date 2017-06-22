1cfe9cd9fa19d0282417d5b4f1e82b89fc80ccc7
olazysk <olazysk94@gmail.com>
2017-06-22 13:43:10 +0200

    SheetMusic - dodano pole "typ utworu"

--- IPNuty/Models/SheetMusic.cs
+++ IPNuty/Models/SheetMusic.cs
@@ -7,13 +7,25 @@ namespace IPNuty.Models
 {
     public class SheetMusic
     {
+
         public string Title { get; set; }
         public string Author { get; set; }
 
-        public SheetMusic(string title, string author)
+        public enum MusicType
+        {
+            rozrywkowe,
+            sakralne,
+            musicalowe,
+            ludowe;
+        }
+
+        public MusicType Type { get; set; }
+
+        public SheetMusic(string title, string author, MusicType Type)
         {
             this.Title = title;
             this.Author = author;
+            this.Type = Type;
         }
     }
 }
